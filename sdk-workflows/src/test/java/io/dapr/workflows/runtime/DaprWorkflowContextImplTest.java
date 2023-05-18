c9ce08bf0456a10fd47e457f798c5ae3d10a93cf
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2023-05-18 10:38:17 -0400

    Initial workflow authoring and example app
    Signed-off-by: Bill DeRusha <444835+bderusha@users.noreply.github.com>

---
+++ sdk-workflows/src/test/java/io/dapr/workflows/runtime/DaprWorkflowContextImplTest.java
@@ -0,0 +1,46 @@
+package io.dapr.workflows.runtime;
+
+import com.microsoft.durabletask.Task;
+import com.microsoft.durabletask.TaskOrchestrationContext;
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Before;
+
+import java.time.Duration;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class DaprWorkflowContextImplTest {
+  private DaprWorkflowContextImpl context;
+
+  @Before
+  public void setUp() {
+    TaskOrchestrationContext mockedInnerContext = mock(TaskOrchestrationContext.class);
+    Task<Void> mockedExternalTask = mock(Task.class);
+
+    when(mockedInnerContext.getInstanceId()).thenReturn("TestInstance");
+    when(mockedInnerContext.getName()).thenReturn("TestName");
+    when(mockedInnerContext.waitForExternalEvent(any(), any(Duration.class))).thenReturn(mockedExternalTask);
+
+    context = new DaprWorkflowContextImpl(mockedInnerContext);
+  }
+
+
+  @Test
+  public void getNameTest() {
+    Assert.assertNotNull(context.getName());
+  }
+
+  @Test
+  public void getInstanceIdTest() {
+    Assert.assertNotNull(context.getInstanceId());
+  }
+
+    @Test
+    public void waitForExternalEventAsyncTest() {
+      Assert.assertNotNull(
+          context.waitForExternalEventAsync("TestEvent", Duration.ofSeconds(1)));
+    }
+}
