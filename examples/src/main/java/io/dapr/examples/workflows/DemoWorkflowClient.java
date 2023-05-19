ec5f1ecf2dce8b9d09bfa5e3badc23ddfa907907
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2023-05-19 13:36:29 -0400

    update example
    Signed-off-by: Bill DeRusha <444835+bderusha@users.noreply.github.com>

--- examples/src/main/java/io/dapr/examples/workflows/DemoWorkflowClient.java
+++ examples/src/main/java/io/dapr/examples/workflows/DemoWorkflowClient.java
@@ -13,12 +13,20 @@ limitations under the License.
 
 package io.dapr.examples.workflows;
 
+import com.microsoft.durabletask.DurableTaskClient;
+import com.microsoft.durabletask.DurableTaskGrpcClientBuilder;
+import com.microsoft.durabletask.NewOrchestrationInstanceOptions;
+import com.microsoft.durabletask.OrchestrationMetadata;
 import io.dapr.actors.ActorId;
 import io.dapr.actors.client.ActorClient;
 import io.dapr.actors.client.ActorProxyBuilder;
+import io.dapr.workflows.runtime.WorkflowRuntime;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Client for Actor runtime to invoke actor methods.
@@ -36,7 +44,21 @@ public class DemoWorkflowClient {
    * @param args Input arguments (unused).
    * @throws InterruptedException If program has been interrupted.
    */
-  public static void main(String[] args) throws InterruptedException {
-    System.out.println("Done.");
+  public static void main(String[] args) throws InterruptedException, TimeoutException {
+    DurableTaskClient client = new DurableTaskGrpcClientBuilder().build();
+    try (client) {
+      String name = DemoWorkflow.class.getCanonicalName();
+      String instanceId = client.scheduleNewOrchestrationInstance(name);
+      System.out.printf("Started new orchestration instance: %s%n", instanceId);
+
+      OrchestrationMetadata completedInstance = client.waitForInstanceCompletion(
+          instanceId,
+          Duration.ofSeconds(30),
+          false);
+
+      System.out.printf("Orchestration completed: %s%n", completedInstance);
+    }
+
+    System.exit(0);
   }
 }
