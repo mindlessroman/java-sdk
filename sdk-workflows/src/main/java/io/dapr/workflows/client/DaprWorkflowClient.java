7608e7b16308e6b90478967e114d89d2766ccd9f
Hannah Kennedy <hakenned@microsoft.com>
2023-05-25 14:18:28 -0400

    Updates from PR review
    Signed-off-by: Hannah Kennedy <hakenned@microsoft.com>

--- sdk-workflows/src/main/java/io/dapr/workflows/client/DaprWorkflowClient.java
+++ sdk-workflows/src/main/java/io/dapr/workflows/client/DaprWorkflowClient.java
@@ -14,7 +14,9 @@ limitations under the License.
 package io.dapr.workflows.client;
 
 import com.microsoft.durabletask.DurableTaskClient;
-import com.microsoft.durabletask.OrchestrationMetadata;
+import com.microsoft.durabletask.DurableTaskGrpcClientBuilder;
+
+import javax.annotation.Nullable;
 
 public class DaprWorkflowClient implements AutoCloseable {
 
@@ -22,41 +24,30 @@ public class DaprWorkflowClient implements AutoCloseable {
 
   /**
    * Constructor for DaprWorkflowClient.
-   *
-   * @param innerClient DurableTaskClient instance
-   * @throws IllegalArgumentException if innerClient is null
    */
-  public DaprWorkflowClient(DurableTaskClient innerClient) throws IllegalArgumentException {
-    if (innerClient == null) {
-      throw new IllegalArgumentException("Inner client cannot be null");
-    } else {
-      this.innerClient = innerClient;
-    }
+  public DaprWorkflowClient() throws IllegalArgumentException {
+    DurableTaskClient innerClient = new DurableTaskGrpcClientBuilder().build();
+    this.innerClient = innerClient;
   }
 
   /**
    * Schedules a new workflow using DurableTask client.
    *
    * @param workflowName name of workflow to start
-   * @return String for new Workflow instance Id
+   * @return String for new Workflow instance id
    */
   public String scheduleNewWorkflow(String workflowName) {
     return this.innerClient.scheduleNewOrchestrationInstance(workflowName);
   }
 
-  //TODO: Overloaded scheduleNewWorkflow implementations
-
-  public OrchestrationMetadata getWorkflowMetadata(String workflowName) {
-    return this.innerClient.getInstanceMetadata(workflowName, false); //TODO: is false what we want?
-  }
-
   /**
    * Terminates the workflow associated with the provided instance id.
    *
-   * @param workflowName name of workflow to terminate
+   * @param workflowInstanceId Workflow instance id to terminate
+   * @param output the optional output to set for the terminated orchestration instance
    */
-  public void terminateWorkflow(String workflowName) {
-    this.innerClient.terminate(workflowName, null); //TODO: should this be null here?
+  public void terminateWorkflow(String workflowInstanceId, @Nullable Object output) {
+    this.innerClient.terminate(workflowInstanceId, output);
   }
 
   /**
@@ -64,7 +55,7 @@ public class DaprWorkflowClient implements AutoCloseable {
    *
    */
   public void close() {
-    if (this.innerClient != null) { //TODO: is there another condition worth checking?
+    if (this.innerClient != null) {
       this.innerClient.close();
     }
 
